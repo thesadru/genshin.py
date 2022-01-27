ac54dfbfd7a510934896967d6507c2eef80451a2
sadru <thesadru@gmail.com>
2022-01-27 18:39:18 +0100

    Add models for a study in potions

--- genshin/models/activities.py
+++ genshin/models/activities.py
@@ -25,7 +25,7 @@ __all__ = [
 
 
 class HyakuninIkkiCharacter(BaseCharacter):
-    """A possibly trial character"""
+    """A Hyakunin Ikki character"""
 
     level: int
     trial: bool = Field(galias="is_trail_avatar")
@@ -115,16 +115,22 @@ class LabyrinthWarriors(GenshinModel):
 
 
 class EnergyAmplifierCharacter(BaseCharacter):
+    """An energy amplifier character"""
+
     level: int
 
 
 class EnergyAmplifierCriteria(GenshinModel):
+    """An energy amplifier challenge criteria"""
+
     id: int
     description: str = Field(galias="desc")
     score: int
 
 
 class EnergyAmplifierBuff(GenshinModel):
+    """An energy amplifier challenge buff"""
+
     id: int
     name: str
     quality: int
@@ -133,6 +139,8 @@ class EnergyAmplifierBuff(GenshinModel):
 
 
 class EnergyAmplifierChallenge(GenshinModel):
+    """An energy amplifier challenge"""
+
     id: int = Field(galias="challenge_id")
     name: str = Field(galias="challenge_name")
     characters: List[EnergyAmplifierCharacter] = Field(galias="avatars")
@@ -145,9 +153,60 @@ class EnergyAmplifierChallenge(GenshinModel):
 
 
 class EnergyAmplifier(GenshinModel):
+    """An energy amplifier event"""
+
     start_time: datetime
     end_time: datetime
     challenges: List[EnergyAmplifierChallenge] = Field(alias="records")
+
+
+# ---------------------------------------------------------
+# A study in potions:
+
+
+class PotionCharacter(BaseCharacter):
+    """A study in potions character"""
+
+    level: int
+    trial: bool = Field(galias="is_trial")
+
+
+class PotionPotion(GenshinModel):
+    """A study in potions potion"""
+
+    id: int
+    name: str
+    quality: int
+    quality_icon: str = Field(galias="icon")
+    mark_icon: str = Field(galias="cornor_mark")
+    description: str = Field(galias="desc")
+
+
+class PotionStage(GenshinModel):
+    """A study in potions stage"""
+
+    name: str = Field(galias="level_name")
+    score: int
+    difficulty: int
+    difficulty_factor: int = Field(galias="factor")
+    characters: List[PotionCharacter] = Field(galias="avatars")
+    potions: List[PotionPotion] = Field(galias="buffs")
+
+
+class PotionChallenge(GenshinModel):
+    """A study in potions challenge"""
+
+    name: str = Field(galias="stage_name")
+    score: int = Field(galias="stage_score")
+    finished: bool
+
+    stages: List[PotionStage] = Field(galias="levels")
+
+
+class Potions(GenshinModel):
+    """A study in potions event"""
+
+    challenges: List[PotionChallenge] = Field(galias="records")
 
 
 # ---------------------------------------------------------
@@ -174,6 +233,7 @@ class Activities(GenshinModel):
     hyakunin_ikki: Optional[HyakuninIkki] = Field(None, gslug="sumo")
     labyrinth_warriors: Optional[LabyrinthWarriors] = Field(None, gslug="rogue")
     energy_amplifier: Optional[EnergyAmplifier] = Field(None, gslug="channeller_slab_copy")
+    potions: Optional[Potions] = Field(None, gslug="potion")
 
     @root_validator(pre=True)
     def __flatten_activities(cls, values: Dict[str, Any]) -> Dict[str, Any]:
