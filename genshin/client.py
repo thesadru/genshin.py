81d0a9aeabc09335a2ef1242142c3488e475094b
sadru <thesadru@gmail.com>
2022-02-15 19:33:21 +0100

    Cope with the new brand name
    L + ratio

--- genshin/client.py
+++ genshin/client.py
@@ -41,17 +41,17 @@ class GenshinClient:
     DS_SALT = "6cqshh5dhw73bzxn20oexa9k516chk7s"
     ACT_ID = "e202102251931481"
 
-    WEBSTATIC_URL = "https://webstatic-sea.mihoyo.com/"
+    WEBSTATIC_URL = "https://webstatic-sea.hoyoverse.com/"
     TAKUMI_URL = "https://api-os-takumi.mihoyo.com/"
-    RECORD_URL = "https://bbs-api-os.mihoyo.com/game_record/"
-    INFO_LEDGER_URL = "https://hk4e-api-os.mihoyo.com/event/ysledgeros/month_info"
-    DETAIL_LEDGER_URL = "https://hk4e-api-os.mihoyo.com/event/ysledgeros/month_detail"
-    CALCULATOR_URL = "https://sg-public-api.mihoyo.com/event/calculateos/"
-    REWARD_URL = "https://hk4e-api-os.mihoyo.com/event/sol/"
-    GACHA_INFO_URL = "https://hk4e-api-os.mihoyo.com/event/gacha_info/api/"
-    YSULOG_URL = "https://hk4e-api-os.mihoyo.com/ysulog/api/"
-    MAP_URL = "https://api-os-takumi-static.mihoyo.com/common/map_user/ys_obc/v1/map/"
-    STATIC_MAP_URL = "https://api-os-takumi-static.mihoyo.com/common/map_user/ys_obc/v1/map"
+    RECORD_URL = "https://bbs-api-os.hoyoverse.com/game_record/"
+    INFO_LEDGER_URL = "https://hk4e-api-os.hoyoverse.com/event/ysledgeros/month_info"
+    DETAIL_LEDGER_URL = "https://hk4e-api-os.hoyoverse.com/event/ysledgeros/month_detail"
+    CALCULATOR_URL = "https://sg-public-api.hoyoverse.com/event/calculateos/"
+    REWARD_URL = "https://hk4e-api-os.hoyoverse.com/event/sol/"
+    GACHA_INFO_URL = "https://hk4e-api-os.hoyoverse.com/event/gacha_info/api/"
+    YSULOG_URL = "https://hk4e-api-os.hoyoverse.com/ysulog/api/"
+    MAP_URL = "https://api-os-takumi-static.hoyoverse.com/common/map_user/ys_obc/v1/map/"
+    STATIC_MAP_URL = "https://api-os-takumi-static.hoyoverse.com/common/map_user/ys_obc/v1/map"
 
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"  # noqa: E501
 
@@ -600,7 +600,7 @@ class GenshinClient:
 
     async def login_with_ticket(self, login_ticket: str = "") -> None:
         """Complete cookies using a login ticket"""
-        url = "https://webapi-os.account.mihoyo.com/Api/cookie_accountinfo_by_loginticket"
+        url = "https://webapi-os.account.hoyoverse.com/Api/cookie_accountinfo_by_loginticket"
         async with self.session.get(url, params=dict(login_ticket=login_ticket)) as r:
             r.raise_for_status()
 
@@ -670,7 +670,7 @@ class GenshinClient:
             server = recognize_server(uid)
             lang = create_short_lang_code(lang or self.lang)
             await self.request(
-                "https://hk4e-api-os.mihoyo.com/common/apicdkey/api/webExchangeCdkey",
+                "https://hk4e-api-os.hoyoverse.com/common/apicdkey/api/webExchangeCdkey",
                 params=dict(
                     uid=uid,
                     region=server,
@@ -1253,7 +1253,7 @@ class GenshinClient:
 
         :param lang: The language to use
         """
-        base = "https://mi18n-os.mihoyo.com/webstatic/admin/mi18n/hk4e_global/"
+        base = "https://mi18n-os.hoyoverse.com/webstatic/admin/mi18n/hk4e_global/"
         data = await self.request_webstatic(base + f"m02251421001311/m02251421001311-{lang}.json")
 
         return {
@@ -1464,13 +1464,13 @@ class ChineseClient(GenshinClient):
     ACT_ID = "e202009291139501"
 
     TAKUMI_URL = "https://api-takumi.mihoyo.com/"
-    RECORD_URL = "https://api-takumi-record.mihoyo.com/game_record/app/"
-    INFO_LEDGER_URL = "https://hk4e-api.mihoyo.com/event/ys_ledger/monthInfo"
-    DETAIL_LEDGER_URL = "https://hk4e-api.mihoyo.com/event/ys_ledger/monthDetail"
-    REWARD_URL = "https://api-takumi.mihoyo.com/event/bbs_sign_reward/"
-    GACHA_INFO_URL = "https://hk4e-api.mihoyo.com/event/gacha_info/api/"
-    MAP_URL = "https://api-takumi-static.mihoyo.com/common/map_user/ys_obc/v1/map/"
-    STATIC_MAP_URL = "https://api-static.mihoyo.com/common/map_user/ys_obc/v1/map"
+    RECORD_URL = "https://api-takumi-record.hoyoverse.com/game_record/app/"
+    INFO_LEDGER_URL = "https://hk4e-api.hoyoverse.com/event/ys_ledger/monthInfo"
+    DETAIL_LEDGER_URL = "https://hk4e-api.hoyoverse.com/event/ys_ledger/monthDetail"
+    REWARD_URL = "https://api-takumi.hoyoverse.com/event/bbs_sign_reward/"
+    GACHA_INFO_URL = "https://hk4e-api.hoyoverse.com/event/gacha_info/api/"
+    MAP_URL = "https://api-takumi-static.hoyoverse.com/common/map_user/ys_obc/v1/map/"
+    STATIC_MAP_URL = "https://api-static.hoyoverse.com/common/map_user/ys_obc/v1/map"
 
     def __init__(
         self,
