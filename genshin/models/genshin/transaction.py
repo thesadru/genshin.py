34576f549c5203a1d616f82bb182f1395aa06d23
xtaodada <xtao@xtaolink.cn>
2023-07-17 12:37:53 +0800

    Fix query genshin transaction log

--- genshin/models/genshin/transaction.py
+++ genshin/models/genshin/transaction.py
@@ -34,19 +34,9 @@ class BaseTransaction(APIModel, Unique):
     kind: TransactionKind
 
     id: int
-    uid: int
-    time: datetime.datetime
+    time: datetime.datetime = Aliased("datetime")
     amount: int = Aliased("add_num")
-    reason_id: int = Aliased("reason")
-
-    @property
-    def reason_name(self) -> str:
-        return self.get_reason_name()
-
-    def get_reason_name(self, lang: typing.Optional[str] = None) -> str:
-        """Get the name of the reason in a specific language."""
-        key = f"inquiry/selfinquiry_general_reason_{self.reason_id}"
-        return self._get_mi18n(key, lang or self.lang, default=str(self.reason_id))
+    reason: str = Aliased("reason")
 
 
 class Transaction(BaseTransaction):
@@ -61,4 +51,4 @@ class ItemTransaction(BaseTransaction):
     kind: typing.Literal[TransactionKind.ARTIFACT, TransactionKind.WEAPON]
 
     name: str
-    rarity: int = Aliased("rank")
+    rarity: int = Aliased("quality")
