873cfac5e28779901c0c699cb1c063c938c489b3
seria <seria.ati@gmail.com>
2024-03-12 08:55:05 +0800

    fix: Fix KeyError if account has never done check-in

--- genshin/client/components/daily.py
+++ genshin/client/components/daily.py
@@ -97,7 +97,7 @@ class DailyRewardClient(base.BaseClient):
         """Get the daily reward info for the current user."""
         data = await self.request_daily_reward("info", game=game, lang=lang)
         return models.DailyRewardInfo(
-            data["is_sign"], data["total_sign_day"], data["sign_cnt_missed"]
+            data["is_sign"], data["total_sign_day"], data.get("sign_cnt_missed", 0)
         )
 
     async def get_monthly_rewards(
