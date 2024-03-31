c07c6a438560ac29764b97415058f3d087c9fa80
seriaati <seria.ati@gmail.com>
2024-03-31 17:02:18 +0900

    feat: Add new ds utilities

--- genshin/utility/ds.py
+++ genshin/utility/ds.py
@@ -9,7 +9,7 @@ import typing
 
 from genshin import constants, types
 
-__all__ = ["generate_cn_dynamic_secret", "generate_dynamic_secret", "get_ds_headers"]
+__all__ = ["generate_cn_dynamic_secret", "generate_dynamic_secret", "get_ds_headers", "generate_passport_ds"]
 
 
 def generate_dynamic_secret(salt: str = constants.DS_SALT[types.Region.OVERSEAS]) -> str:
@@ -59,3 +59,53 @@ def get_ds_headers(
     else:
         raise TypeError(f"{region!r} is not a valid region.")
     return ds_headers
+
+
+_S = {
+    "2.60.1": {
+        "K2": "AcpNVhfh0oedCobdCyFV8EE1jMOVDy9q",
+        "LK2": "1OJyMNCqFlstEQqqMOv0rKCIdTOoJhNt",
+        "22": "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
+        "25": "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs",
+    },
+    "os": "6cqshh5dhw73bzxn20oexa9k516chk7s",
+    "PD": "JwYDpKvLj6MrMqqYU6jTKF17KNO2PXoS",
+}
+
+
+def md5(text: str):
+    md5_func = hashlib.md5()
+    md5_func.update(text.encode())
+    return md5_func.hexdigest()
+
+
+def _random_str_ds(
+    salt: str,
+    sets: str = string.ascii_lowercase + string.digits,
+    with_body: bool = False,
+    q: str = "",
+    b: typing.Mapping[str, typing.Any] | None = None,
+):
+    i = str(int(time.time()))
+    r = "".join(random.sample(sets, 6))
+    s = f"salt={salt}&t={i}&r={r}"
+    if with_body:
+        s += f'&b={json.dumps(b) if b else ""}&q={q}'
+    c = md5(s)
+    return f"{i},{r},{c}"
+
+
+def generate_passport_ds(q: str = "", b: typing.Mapping[str, typing.Any] | None = None):
+    r = _random_str_ds(_S["PD"], string.ascii_letters, True, q, b)
+    return r
+
+
+# def generate_passport_ds(body: typing.Mapping[str, typing.Any]) -> str:
+#     """Create a dynamic secret for Miyoushe passport API."""
+#     salt = constants.DS_SALT["cn_passport"]
+#     t = int(time.time())
+#     r = "".join(random.sample(string.ascii_letters, 6))
+#     b = json.dumps(body)
+#     h = hashlib.md5(f"salt={salt}&t={t}&r={r}&b={b}&q=".encode()).hexdigest()
+#     result = f"{t},{r},{h}"
+#     return result
