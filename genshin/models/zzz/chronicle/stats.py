053c601d7d82e1396c894694b89fa8cf30584600
seria <seria.ati@gmail.com>
2024-07-05 10:46:31 +0900

    Add buddy_list

--- genshin/models/zzz/chronicle/stats.py
+++ genshin/models/zzz/chronicle/stats.py
@@ -22,8 +22,19 @@ class ZZZStats(APIModel):
     bangboo_obtained: int = Aliased("buddy_num")
 
 
+class ZZZBaseBangboo(APIModel):
+    """Base bangboo (buddy) model."""
+
+    id: int
+    name: str
+    rarity: typing.Literal["S", "A"]
+    level: int
+    star: int
+
+
 class ZZZUserStats(APIModel):
     """Zenless Zone Zero user model."""
 
     stats: ZZZStats
     agents: typing.Sequence[ZZZPartialAgent] = Aliased("avatar_list")
+    bangboos: typing.Sequence[ZZZBaseBangboo] = Aliased("buddy_list")
