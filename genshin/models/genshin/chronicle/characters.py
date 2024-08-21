710bf7f3e5331a3f5376ed57e4277ec20d4ef64c
Furia <83609040+FuriaPaladins@users.noreply.github.com>
2024-08-21 11:56:56 +0100

    Implement detailed Genshin character endpoint (#219)
    * Implement detailed Genshin character endpoint.
    
    * Run nox and fix formatting issues.
    
    * Fix type checker issues
    
    * Replace root_validator with validator
    
    * Improve __fill_prop_info
    
    * Rename some fields and models
    
    * Improve artifact set effect impl
    
    ---------
    
    Co-authored-by: seriaati <seria.ati@gmail.com>

--- genshin/models/genshin/chronicle/characters.py
+++ genshin/models/genshin/chronicle/characters.py
@@ -1,6 +1,8 @@
 """Genshin chronicle character."""
 
+import enum
 import typing
+from collections import defaultdict
 
 if typing.TYPE_CHECKING:
     import pydantic.v1 as pydantic
@@ -15,14 +17,34 @@ from genshin.models.model import Aliased, APIModel, Unique
 
 __all__ = [
     "Artifact",
+    "ArtifactProperty",
     "ArtifactSet",
     "ArtifactSetEffect",
     "Character",
+    "CharacterSkill",
     "CharacterWeapon",
     "Constellation",
+    "DetailArtifact",
+    "DetailCharacterWeapon",
+    "GenshinDetailCharacter",
+    "GenshinDetailCharacters",
+    "GenshinWeaponType",
     "Outfit",
     "PartialCharacter",
+    "PropInfo",
+    "PropertyValue",
+    "SkillAffix",
 ]
+
+
+class GenshinWeaponType(enum.IntEnum):
+    """Character weapon types."""
+
+    SWORD = 1
+    CATALYST = 10
+    CLAYMORE = 11
+    BOW = 12
+    POLEARM = 13
 
 
 class PartialCharacter(character.BaseCharacter):
@@ -50,13 +72,9 @@ class CharacterWeapon(APIModel, Unique):
 class ArtifactSetEffect(APIModel):
     """Effect of an artifact set."""
 
-    pieces: int = Aliased("activation_number")
+    required_piece_num: int = Aliased("activation_number")
     effect: str
-    enabled: bool = False
-
-    class Config:
-        # this is for the "enabled" field, hopefully nobody abuses this
-        allow_mutation = True
+    active: bool = Aliased("enabled", default=False)
 
 
 class ArtifactSet(APIModel, Unique):
@@ -113,14 +131,168 @@ class Character(PartialCharacter):
     outfits: typing.Sequence[Outfit] = Aliased("costumes")
 
     @pydantic.validator("artifacts")
-    def __add_artifact_effect_enabled(cls, artifacts: typing.Sequence[Artifact]) -> typing.Sequence[Artifact]:
-        sets: typing.Dict[int, typing.List[Artifact]] = {}
+    @classmethod
+    def __enable_artifact_set_effects(cls, artifacts: typing.Sequence[Artifact]) -> typing.Sequence[Artifact]:
+        set_nums: typing.DefaultDict[int, int] = defaultdict(int)
         for arti in artifacts:
-            sets.setdefault(arti.set.id, []).append(arti)
+            set_nums[arti.set.id] += 1
 
         for artifact in artifacts:
             for effect in artifact.set.effects:
-                if effect.pieces <= len(sets[artifact.set.id]):
-                    effect.enabled = True
+                if effect.required_piece_num <= set_nums[artifact.set.id]:
+                    effect.active = True
 
         return artifacts
+
+
+class PropInfo(APIModel):
+    """A property such as Crit Rate, HP, HP%."""
+
+    type: int = Aliased("property_type")
+    name: str
+    icon: typing.Optional[str]
+    filter_name: str
+
+    @pydantic.validator("name", "filter_name")
+    @classmethod
+    def __fix_names(cls, value: str) -> str:
+        r"""Fix "\xa0" in Crit Damage + Crit Rate names."""
+        return value.replace("\xa0", " ")
+
+
+class PropertyValue(APIModel):
+    """A property with a value."""
+
+    base: str
+    add: str
+    final: str
+    info: PropInfo
+
+
+class DetailCharacterWeapon(CharacterWeapon):
+    """Detailed Genshin Weapon with main/sub stats."""
+
+    main_stat: PropertyValue = Aliased("main_property")
+    sub_stat: typing.Optional[PropertyValue] = Aliased("sub_property")
+
+
+class ArtifactProperty(APIModel):
+    """Artifact's Property value & roll count."""
+
+    value: str
+    times: int
+    info: PropInfo
+
+
+class DetailArtifact(Artifact):
+    """Detailed artifact with main/sub stats."""
+
+    main_stat: ArtifactProperty = Aliased("main_property")
+    sub_stats: typing.Sequence[ArtifactProperty] = Aliased("sub_property_list")
+
+
+class SkillAffix(APIModel):
+    """Skill affix texts."""
+
+    name: str
+    value: str
+
+
+class CharacterSkill(APIModel):
+    """Character's skill."""
+
+    id: int = Aliased("skill_id")
+    skill_type: int
+    name: str
+    level: int
+
+    description: str = Aliased("desc")
+    affixes: typing.Sequence[SkillAffix] = Aliased("skill_affix_list")
+    icon: str
+    is_unlocked: bool = Aliased("is_unlock")
+
+
+class GenshinDetailCharacter(PartialCharacter):
+    """Full Detailed Genshin Character"""
+
+    is_chosen: bool
+
+    # display_image is a different image that is returned by the full character endpoint, but it is not the full gacha art.
+    display_image: str = Aliased("image")
+
+    weapon_type: GenshinWeaponType
+    weapon: DetailCharacterWeapon
+
+    costumes: typing.Sequence[Outfit]
+
+    artifacts: typing.Sequence[DetailArtifact] = Aliased("relics")
+    constellations: typing.Sequence[Constellation]
+
+    skills: typing.Sequence[CharacterSkill]
+
+    selected_properties: typing.Sequence[PropertyValue]
+    base_properties: typing.Sequence[PropertyValue]
+    extra_properties: typing.Sequence[PropertyValue]
+    element_properties: typing.Sequence[PropertyValue]
+
+
+class GenshinDetailCharacters(APIModel):
+    """Genshin character list."""
+
+    characters: typing.Sequence[GenshinDetailCharacter] = Aliased("list")
+
+    property_map: typing.Mapping[str, PropInfo]
+    possible_artifact_stats: typing.Mapping[str, typing.Sequence[PropInfo]] = Aliased("relic_property_options")
+
+    artifact_wiki: typing.Mapping[str, str] = Aliased("relic_wiki")
+    weapon_wiki: typing.Mapping[str, str]
+    avatar_wiki: typing.Mapping[str, str]
+
+    @pydantic.root_validator(pre=True)
+    def __fill_prop_info(cls, values: typing.Dict[str, typing.Any]) -> typing.Mapping[str, typing.Any]:
+        """Fill property info from properety_map."""
+        relic_property_options: typing.Dict[str, list[int]] = values.get("possible_artifact_stats", {})
+        prop_map: typing.Dict[str, typing.Dict[str, typing.Any]] = values.get("property_map", {})
+        characters: list[typing.Dict[str, typing.Any]] = values.get("characters", [])
+
+        # Map properties to artifacts
+        new_relic_prop_options: typing.Dict[str, list[typing.Dict[str, typing.Any]]] = {}
+        for relic_type, properties in relic_property_options.items():
+            formatted_properties: list[typing.Dict[str, typing.Any]] = [
+                prop_map[str(prop)] for prop in properties if str(prop) in prop_map
+            ]
+            new_relic_prop_options[relic_type] = formatted_properties
+
+        # Override possible_artifact_stats
+        values["possible_artifact_stats"] = new_relic_prop_options
+
+        for char in characters:
+            # Extract character info from .base
+            for key, value in char["base"].items():
+                if key == "weapon":  # Ignore .weapon in base as it does not have full info.
+                    continue
+                char[key] = value
+
+            # Map properties to main/sub stat for weapon.
+            main_property = char["weapon"]["main_property"]
+            char["weapon"]["main_property"]["info"] = prop_map[str(main_property["property_type"])]
+            if sub_property := char["weapon"]["sub_property"]:
+                char["weapon"]["sub_property"]["info"] = prop_map[str(sub_property["property_type"])]
+
+            # Map properties to artifacts
+            for artifact in char["relics"]:
+                main_property = artifact["main_property"]
+                artifact["main_property"]["info"] = prop_map[str(main_property["property_type"])]
+                for sub_property in artifact["sub_property_list"]:
+                    sub_property["info"] = prop_map[str(sub_property["property_type"])]
+
+            # Map character properties
+            for prop in (
+                char["base_properties"]
+                + char["selected_properties"]
+                + char["extra_properties"]
+                + char["element_properties"]
+            ):
+                prop["info"] = prop_map[str(prop["property_type"])]
+
+        return values
