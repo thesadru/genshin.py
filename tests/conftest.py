53aab828eeb51cf57a75fe7a54aaedc648846307
seriaati <seria.ati@gmail.com>
2024-08-25 19:58:54 +0800

    Change event_loop scope to session

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,12 @@ import pytest
 
 import genshin
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     with warnings.catch_warnings():
-#         warnings.simplefilter("ignore")
 
-#         loop = asyncio.get_event_loop()
-
-#     yield loop
-#     loop.close()
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="session")
