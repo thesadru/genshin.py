1603e1e88b84d276ffc36a700a8db7d43c1db186
seria <seria.ati@gmail.com>
2024-09-26 23:36:45 +0800

    Migrate the project to uv (#226)
    * Migrate the project to uv
    
    * Fix some tests failing
    
    * Fix oopsie
    
    * Add aioredis and aiosqlite to all extra
    
    * me when im dumb

--- noxfile.py
+++ noxfile.py
@@ -13,6 +13,7 @@ nox.options.reuse_existing_virtualenvs = True
 PACKAGE = "genshin"
 GENERAL_TARGETS = ["./noxfile.py", "./genshin", "./tests"]
 PYRIGHT_ENV = {"PYRIGHT_PYTHON_FORCE_VERSION": "latest"}
+UV_RUN_EXTRA = ("uv", "run", "--isolated", "--no-dev", "--extra")
 
 LOGGER = logging.getLogger("nox")
 
@@ -27,46 +28,32 @@ def verbose_args() -> typing.Sequence[str]:
     return ["--verbose"] if isverbose() else []
 
 
-def install_requirements(session: nox.Session, *requirements: str, literal: bool = False) -> None:
-    """Install requirements."""
-    if not literal and all(requirement.isalpha() for requirement in requirements):
-        files = ["requirements.txt"] + [f"./genshin-dev/{requirement}-requirements.txt" for requirement in requirements]
-        requirements = ("pip", *tuple(arg for file in files for arg in ("-r", file)))
-
-    session.install("--upgrade", *requirements, silent=not isverbose())
-
-
 @nox.session()
 def docs(session: nox.Session) -> None:
     """Generate docs for this project using Pdoc."""
-    install_requirements(session, "docs")
-
     output_directory = pathlib.Path("./docs/pdoc/")
     session.log("Building docs into %s", output_directory)
 
-    session.run("pdoc3", "--html", PACKAGE, "-o", str(output_directory), "--force")
+    session.run(*UV_RUN_EXTRA, "docs", "pdoc3", "--html", PACKAGE, "-o", str(output_directory), "--force")
     session.log("Docs generated: %s", output_directory / "index.html")
 
 
 @nox.session()
 def lint(session: nox.Session) -> None:
     """Run this project's modules against the pre-defined flake8 linters."""
-    install_requirements(session, "lint")
-    session.run("ruff", "check", *GENERAL_TARGETS, *verbose_args())
+    session.run(*UV_RUN_EXTRA, "lint", "ruff", "check", *GENERAL_TARGETS, *verbose_args())
 
 
 @nox.session()
 def reformat(session: nox.Session) -> None:
     """Reformat this project's modules to fit the standard style."""
-    install_requirements(session, "reformat")
-    session.run("python", "-m", "black", *GENERAL_TARGETS, *verbose_args())
+    session.run(*UV_RUN_EXTRA, "reformat", "black", *GENERAL_TARGETS, *verbose_args())
     # sort __all__ and format imports
     session.run(
-        "python",
-        "-m",
+        *UV_RUN_EXTRA,
+        "reformat",
         "ruff",
         "check",
-        "--preview",
         "--select",
         "RUF022,I",
         "--fix",
@@ -74,15 +61,17 @@ def reformat(session: nox.Session) -> None:
         *verbose_args(),
     )
     # fix all fixable linting errors
-    session.run("ruff", "check", "--fix", *GENERAL_TARGETS, *verbose_args())
+    session.run(*UV_RUN_EXTRA, "reformat", "ruff", "check", "--fix", *GENERAL_TARGETS, *verbose_args())
 
 
 @nox.session(name="test")
 def test(session: nox.Session) -> None:
     """Run this project's tests using pytest."""
-    install_requirements(session, "pytest")
-
     session.run(
+        *UV_RUN_EXTRA,
+        "pytest",
+        "--extra",
+        "all",
         "pytest",
         "--asyncio-mode=auto",
         "-r",
@@ -103,18 +92,25 @@ def test(session: nox.Session) -> None:
 @nox.session(name="type-check")
 def type_check(session: nox.Session) -> None:
     """Statically analyse and veirfy this project using pyright and mypy."""
-    install_requirements(session, "typecheck")
-    session.run("pyright", PACKAGE, *verbose_args(), env=PYRIGHT_ENV)
-    session.run("mypy", PACKAGE, *verbose_args())
+    session.run(*UV_RUN_EXTRA, "typecheck", "--extra", "all", "pyright", PACKAGE, *verbose_args(), env=PYRIGHT_ENV)
+    session.run(*UV_RUN_EXTRA, "typecheck", "--extra", "all", "mypy", PACKAGE, *verbose_args())
 
 
 @nox.session(name="verify-types")
 def verify_types(session: nox.Session) -> None:
     """Verify the "type completeness" of types exported by the library using pyright."""
-    install_requirements(session, ".", "--force-reinstall", "--no-deps")
-    install_requirements(session, "typecheck")
-
-    session.run("pyright", "--verifytypes", PACKAGE, "--ignoreexternal", *verbose_args(), env=PYRIGHT_ENV)
+    session.run(
+        *UV_RUN_EXTRA,
+        "typecheck",
+        "--extra",
+        "all",
+        "pyright",
+        "--verifytypes",
+        PACKAGE,
+        "--ignoreexternal",
+        *verbose_args(),
+        env=PYRIGHT_ENV,
+    )
 
 
 @nox.session(python=False)
