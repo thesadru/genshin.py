f101631a31aa73f50c72ac34c826dba2e38f63fb
seria <seria.ati@gmail.com>
2025-04-22 20:40:04 +0900

    fix: Hopefully this fixes the issue

--- genshin/models/genshin/lineup.py
+++ genshin/models/genshin/lineup.py
@@ -178,7 +178,7 @@ class LineupScenario(APIModel, Unique):
         """Name certain scenarios."""
         scenario_ids = {  # type: ignore
             field.json_schema_extra["scenario_id"]: name
-            for name, field in LineupScenario.model_fields.items()
+            for name, field in cls.model_fields.items()
             if isinstance(field.json_schema_extra, dict) and field.json_schema_extra.get("scenario_id")
         }
 
